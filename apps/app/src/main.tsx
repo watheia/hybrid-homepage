34c440e34b62bf3b9118f9f4de9c21b1c574e77d
Aaron R Miller <amiller@watheia.org>
2021-08-22 08:26:50 +0000

    add material ui provider

--- apps/app/src/main.tsx
+++ apps/app/src/main.tsx
@@ -7,12 +7,43 @@ import { Theme } from "@hybrid-homepage/iron-ui"
 
 import { BrowserRouter } from "react-router-dom"
 
+import { createTheme, responsiveFontSizes, ThemeProvider } from "@material-ui/core/styles"
+import { CssBaseline } from "@material-ui/core"
+
+const theme = responsiveFontSizes(
+  createTheme({
+    palette: {
+      primary: {
+        light: "#9be5e5",
+        main: "#04BFBF",
+        dark: "#038f8f"
+      },
+      secondary: {
+        light: "#9ab6c7",
+        main: "#024873",
+        dark: "#012b45"
+      },
+      text: {
+        primary: "#110f10",
+        secondary: "#231f20"
+      },
+      background: {
+        default: "#fafbfb",
+        paper: "#fefeff"
+      }
+    }
+  })
+)
+
 ReactDOM.render(
   <StrictMode>
     <BrowserRouter>
-      <Theme>
-        <App />
-      </Theme>
+      <ThemeProvider theme={theme}>
+        <Theme>
+          <CssBaseline />
+          <App />
+        </Theme>
+      </ThemeProvider>
     </BrowserRouter>
   </StrictMode>,
   document.getElementById("root")
